a7da1e6a4c924ce0819564849e5a3c0ef3cf8ad6
David Meese <github@myrddins.com>
2012-10-13 10:13:43 -0400

    Initial Comit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MathWorld::Application.config.secret_token = 'b2d714d70448fc04b383e44df7e6e43b0cc544b42134a11867913d98d423c582f149304578d75086d24f0b6437ccf9548a76492acee55a687aa03cbfd411ee04'
