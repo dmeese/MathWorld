df50b7fc049c25863bccd4661ba3b9f538fe8778
sck-nogas <sck@nogas.org>
2012-10-21 14:15:15 -0700

    SCK created stripify

---
+++ db/stripify.rb
@@ -0,0 +1,15 @@
+# Function to strip all special characters from the user_input. 
+
+def stripify(input_string)
+	output = input_string.tr(%q{;`"'}, '') 
+	return output 
+end
+
+/* Un comment to test
+
+my_string="This is an 'evil' string with \"lots\" of `nasty` things; in it..."
+print my_string
+print
+print stripify(my_string)
+
+*/ 
