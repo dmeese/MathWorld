fa714195c615d83dd65d80cfa39c45f1fbc32412
Chris Knight <christopher.knight@att.net>
2012-10-25 19:55:52 -0400

    Moved User.rb to app/models
    Where it belongs

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,32 @@
+require 'bcrypt'
+=begin
+class User connects to the user records in the underlying database through
+Rails' ActiveRecord functionality - this hides a lot of the implementation
+from us, letting us focus on just what we want to do without dealing with
+the database CRUD
+
+The bcrypt gem gives us strong hashing with salts built in
+=end
+
 class User < ActiveRecord::Base
-  # attr_accessible :title, :body
+  include BCrypt
+
+  attr_accessor :UserId, :AuthorizationLevel, :FailedAttempts, :Email
+  attr_reader :UserIndex
+
+  #initialize is Ruby's ctor
+  def initialize(attribute = {})
+    @UserIndex = attributes[:UserIndex]
+    @UserId = attributes[:UserId]
+    @Password = Password.create(attributes[:Password])
+    @AuthorizationLevel = attributes[:AuthorizationLevel]
+    @FailedAttempts = attributes[:FailedAttempts]
+    @Email = attributes[:Email]
+  end
+
+  #self makes this a class method.  This should return an initialized User object
+  # on successful authentication, and nil on failure.
+  def self.authenticate_safely(userId, password)
+    where("userId = ? AND password = ?", user_name, Password.create(password)).first
+  end
 end
