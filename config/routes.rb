c8e3be0ad01b3fa5ff1b41a83aeb1fd8706ebb95
David Meese <github@myrddins.com>
2012-10-28 00:20:23 -0400

    Changed inital root
    removed public/index.html static page and changed root in routes.db to
    point to the oublic view.

--- config/routes.rb
+++ config/routes.rb
@@ -56,7 +56,7 @@ MathWorld::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'math_content#public'
 
   # See how all your routes lay out with "rake routes"
 
