4f302f629a80075bedec2f64390c013943fc1915
Chris Knight <christopher.knight@att.net>
2012-11-03 10:53:06 -0400

    Fix destory and other things I failed to upload

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,15 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
 
   def create
+    @user = User.new(params[:user])
+    if @user.save
+      redirect_to :action => 'index'
+    else
+      render 'new'
+    end
   end
 
   def index
@@ -13,11 +20,24 @@ class UsersController < ApplicationController
   end
 
   def edit
+    @user = User.find(params[:id])
   end
 
   def update
+    #return render :text => params
+    @user = User.find(params[:user][:id])
+    if @user.update_attributes(params[:user])
+      redirect_to :action => 'index'
+    else
+      render 'edit'
+    end
   end
 
   def destroy
+    @user = User.find(params[:id])
+    if (@user)
+      @user.destroy
+    end
+    redirect_to :action => 'index'
   end
 end
