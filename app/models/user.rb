6cd6bd1f30d03243e8e3b875975bfe6275aa9828
Chris Knight <christopher.knight@att.net>
2012-11-04 17:19:25 -0500

    Removed unused file; added comments
    the model Session was removed when it was determined that Rails sessions
    would be sufficient for our needs.

--- app/models/user.rb
+++ app/models/user.rb
@@ -22,9 +22,23 @@ The has_secure_password method gives us strong hashing via the bcrypt gem with s
 =end
 
 class User < ActiveRecord::Base
+
+  #Make all the fields mass-assignable (such as during construction)
   attr_accessible :UserID, :UserName, :password, :password_confirmation, :AuthorizationLevel
+  
+  #Verify that password was entered and is at least 6 characters long
   validates :password, presence: true, length: { minimum: 6 }
+  
+  #The next two methods come from ActiveRecord.  They provide for the use
+  #of cleartext passwords from the web app, with confirmation, and a digest being
+  #saved in the database.  The digesting is performed using BCrypt.
+  #To use has_secure_password, the database field must be named "password_digest".
+  #Two methods are then exposed from User: password and password_confirmation
+  #This is all handled automagically by ActiveRecord
   has_secure_password
   validates_confirmation_of :password
+  
+  #Sets up foreign key relationship with the documents table/Document model object.
+  #This object is the parent record; the foreign key field in documents defaults to user_id.
   has_many :documents
 end
