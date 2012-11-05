a09319eadd197d0ea68f8bb9ffde9ef6877153cd
Scott C. Kennedy <sck@nogas.org>
2012-11-04 20:33:37 -0800

    Fixed docuemnt controller to only allow actions when authorized users are logged in and only show documents when required.

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -15,70 +15,94 @@ class DocumentsController < ApplicationController
   def show
     @document = Document.find(params[:id])
 
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @document }
+    if (@loggedinuser && @loggedinuser.AuthorizationLevel >= 1 ) || @document.AuthorizationLevel == 1 
+      respond_to do |format|
+        format.html # show.html.erb
+        format.json { render json: @document }
+      end
+    else
+       redirect_to '/'
     end
   end
 
   # GET /documents/new
   # GET /documents/new.json
   def new
-    @document = Document.new
+    if @loggedinuser && @loggedinuser.AuthorizationLevel >= 3
+       @document = Document.new
 
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @document }
+      respond_to do |format|
+        format.html # new.html.erb
+        format.json { render json: @document }
+      end
+    else
+       redirect_to '/'
     end
   end
 
   # GET /documents/1/edit
   def edit
-    @document = Document.find(params[:id])
+    if @loggedinuser && @loggedinuser.AuthorizationLevel >= 3
+      @document = Document.find(params[:id])
+    else
+       redirect_to '/'
+    end
   end
 
   # POST /documents
   # POST /documents.json
   def create
-    @document = Document.new(params[:document])
-    @document.Owner = @loggedinuser.UserName
-
-    respond_to do |format|
-      if @document.save
-        format.html { redirect_to @document, notice: 'Document was successfully created.' }
-        format.json { render json: @document, status: :created, location: @document }
-      else
-        format.html { render action: "new" }
-        format.json { render json: @document.errors, status: :unprocessable_entity }
+    if @loggedinuser && @loggedinuser.AuthorizationLevel >= 3
+      @document = Document.new(params[:document])
+      @document.Owner = @loggedinuser.UserName
+  
+      respond_to do |format|
+        if @document.save
+          format.html { redirect_to @document, notice: 'Document was successfully created.' }
+          format.json { render json: @document, status: :created, location: @document }
+        else
+          format.html { render action: "new" }
+          format.json { render json: @document.errors, status: :unprocessable_entity }
+        end
       end
+    else
+       redirect_to '/'
     end
   end
 
   # PUT /documents/1
   # PUT /documents/1.json
   def update
-    @document = Document.find(params[:id])
+    if @loggedinuser && @loggedinuser.AuthorizationLevel >= 3
+      @document = Document.find(params[:id])
 
-    respond_to do |format|
-      if @document.update_attributes(params[:document])
-        format.html { redirect_to @document, notice: 'Document was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: "edit" }
-        format.json { render json: @document.errors, status: :unprocessable_entity }
+      respond_to do |format|
+        if @document.update_attributes(params[:document])
+          format.html { redirect_to @document, notice: 'Document was successfully updated.' }
+          format.json { head :no_content }
+        else
+          format.html { render action: "edit" }
+          format.json { render json: @document.errors, status: :unprocessable_entity }
+        end
       end
+    else
+       redirect_to '/'
     end
   end
 
   # DELETE /documents/1
   # DELETE /documents/1.json
   def destroy
-    @document = Document.find(params[:id])
-    @document.destroy
+    if @loggedinuser && @loggedinuser.AuthorizationLevel >= 3
+      @document = Document.find(params[:id])
+      @document.destroy
 
-    respond_to do |format|
-      format.html { redirect_to documents_url }
-      format.json { head :no_content }
+      respond_to do |format|
+        format.html { redirect_to documents_url }
+        format.json { head :no_content }
+      end
+    else
+       redirect_to '/'
     end
   end
 end
