1b182ca3da54009ffb299cf96d6fb6f9721dd49b
David Meese <github@myrddins.com>
2012-11-18 11:27:26 -0500

    Added DB columns to Docs and created rating form for show

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121106170540) do
+ActiveRecord::Schema.define(:version => 20121118161853) do
 
   create_table "documents", :force => true do |t|
     t.string   "filename"
@@ -22,6 +22,8 @@ ActiveRecord::Schema.define(:version => 20121106170540) do
     t.datetime "created_at",         :null => false
     t.datetime "updated_at",         :null => false
     t.integer  "user_id"
+    t.float    "rating_avg"
+    t.integer  "rating_count"
   end
 
   create_table "sessions", :force => true do |t|
