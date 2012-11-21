a1e339543fd6611f6213eb75e74f6430597a6953
Scott C. Kennedy <sck@nogas.org>
2012-11-20 16:46:47 -0800

    Added the stripify of the comments input.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -10,10 +10,22 @@
 #  updated_at  :datetime         not null
 #
 
+require 'stripify' 
+
 class Comment < ActiveRecord::Base
   attr_accessible :body, :commenter, :document_id
 
   #belongs_to is an ActiveRecord method which establishes a 
   #foreign key relationship, with this record as the child
   belongs_to :document, :foreign_key => :id
+
+
+  #set up remove_evil as an event callback, to be called before
+  #the record is committed to the data repository.
+  before_save :remove_evil
+
+  def remove_evil()
+    self.body = Stripify::stripify(self.body)
+  end
+
 end
